c804336c1ccfb9686b785d8cb08f5b25acc8bb1d
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-11 13:06:45 +1000

    feat: se agregan proveedores de Auth0 con historial y navegación

---
+++ client/src/auth/auth0Provider.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa el hook de navegación de React Router
+import { Auth0Provider } from '@auth0/auth0-react'; // Importa el proveedor de Auth0 para React
+
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN; // Obtiene el dominio de Auth0 de las variables de entorno
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID; // Obtiene el ID del cliente de Auth0 de las variables de entorno
+
+  const navigate = useNavigate(); // Hook para navegar entre rutas
+
+  // Función de callback para manejar redirecciones después de la autenticación
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || window.location.pathname); // Navega a la ruta almacenada en appState o a la ruta actual
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain} 
+      clientId={clientId}
+      redirectUri={window.location.origin} // Configura la URI de redirección después de la autenticación
+      onRedirectCallback={onRedirectCallback} // Asigna la función de callback de redirección
+    >
+      {children} {/* Renderiza los componentes hijos dentro del proveedor de Auth0 */}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory; // Exporta el componente Auth0ProviderWithHistory
