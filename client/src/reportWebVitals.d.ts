0adc7e614740e42262b7f2777121d2f9e498ab65
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-11 13:00:45 +1000

    chore: convertido de JS a TSX

---
+++ client/src/reportWebVitals.d.ts
@@ -0,0 +1,7 @@
+declare module 'reportWebVitals' {
+    import { ReportHandler } from 'web-vitals';
+  
+    const reportWebVitals: (onPerfEntry?: ReportHandler) => void;
+    export default reportWebVitals;
+  }
+  
