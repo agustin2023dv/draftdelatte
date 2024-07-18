523b8170b479dd36496d021e5c966c9ff21ce8d2
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-19 06:01:15 +1000

    feat: añadir rutas para registro, inicio de sesión y recuperación de contraseña
    Añadidas nuevas rutas en el archivo src/index.tsx para manejar las páginas de registro (SignUp), inicio de sesión (Login) y recuperación de contraseña (ForgotPassword). Este cambio también incluye la configuración del proveedor de Auth0 y el uso de React Router para la navegación entre estas páginas.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,15 @@
 // src/index.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Importa el archivo CSS principal
 import App from './App'; // Importa el componente principal de la aplicación
-import Auth from './pages/Auth/Auth'; // Importa la página de autenticación
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importa componentes de React Router
-import { Auth0Provider } from '@auth0/auth0-react'; // Importa el proveedor de Auth0
+import SignUp from 'pages/SignUp/SignUp';
+import Login from 'pages/Login/Login';
+import ForgotPassword from 'pages/ForgotPassword/ForgotPassword';
 
-// Obtiene el dominio y el ID del cliente de Auth0 desde las variables de entorno
-const domain = process.env.REACT_APP_AUTH0_DOMAIN!;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID!;
-const redirectUri = window.location.origin; // La URL a la que Auth0 redirige después de la autenticación
 
-// Verifica que las variables de entorno estén definidas
-if (!domain || !clientId) {
-  throw new Error('Las variables de entorno REACT_APP_AUTH0_DOMAIN y REACT_APP_AUTH0_CLIENT_ID deben estar definidas.');
-}
+
 
 // Obtiene el elemento root del DOM
 const rootElement = document.getElementById('root');
@@ -30,20 +23,19 @@ const root = ReactDOM.createRoot(rootElement);
 // Renderiza la aplicación
 root.render(
   <React.StrictMode>
-    {/* Configura el proveedor de Auth0 */}
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
-    >
+
+
+ 
+    
       {/* Configura el enrutador de la aplicación */}
       <Router>
         <Routes>
-          {/* Define las rutas de la aplicación */}
+
           <Route path="/" element={<App />} /> 
-          <Route path="/auth" element={<Auth />} /> 
+          <Route path="/login" element={<Login/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </Router>
-    </Auth0Provider>
   </React.StrictMode>
 );
