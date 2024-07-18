06ece334f7b8b39fdf7e9424e8493de0a7b7625d
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-19 05:30:13 +1000

    delete: archivos innecesarios

--- client/src/reportWebVitals.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'reportWebVitals' {
-    import { ReportHandler } from 'web-vitals';
-  
-    const reportWebVitals: (onPerfEntry?: ReportHandler) => void;
-    export default reportWebVitals;
-  }
-  
