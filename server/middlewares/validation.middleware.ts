da79394097efcab7064b2114787113930d52e3ad
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-19 22:07:23 +1000

    feat: añadir middleware de validación para registro y login
    Añadido middleware de validación para las rutas de registro y login utilizando express-validator. Las validaciones aseguran que los campos de correo electrónico y contraseña cumplan con los requisitos especificados y que los campos de nombre y apellido no estén vacíos. Si las validaciones fallan, se devuelve un error con los detalles de la validación.

---
+++ server/middlewares/validation.middleware.ts
@@ -0,0 +1,37 @@
+// validationMiddleware.ts
+import { body, validationResult } from 'express-validator';
+
+// Caracteres especiales permitidos en el email
+const emailRegex = /^[^@]+@\w+(\.\w+)+\w$/;
+
+//**Validación para el registro
+export const registerValidation = [
+  body('email').isEmail().withMessage('Ingrese un correo electrónico válido')
+    .matches(emailRegex).withMessage('El correo electrónico contiene caracteres inválidos'),
+  body('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+    .withMessage('La contraseña debe incluir al menos una letra mayúscula, una minúscula, un número y un símbolo especial'),
+  body('firstName').not().isEmpty().trim().escape().withMessage('El nombre es obligatorio'),
+  body('lastName').not().isEmpty().trim().escape().withMessage('El apellido es obligatorio'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+//**Validación para el login
+export const loginValidation = [
+  body('email').isEmail().withMessage('Ingrese un correo electrónico válido')
+    .matches(emailRegex).withMessage('El correo electrónico contiene caracteres inválidos'),
+  body('password').not().isEmpty().withMessage('La contraseña es obligatoria'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
