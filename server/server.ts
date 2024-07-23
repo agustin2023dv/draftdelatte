974f9ab2e2102b069226c121001ee875e9aeee8c
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-23 10:46:27 +1000

    refactor: mejorar configuración del servidor y servir frontend estático
    Actualizada la configuración del servidor Express para incluir middleware de análisis del cuerpo de solicitudes, conectar a MongoDB, definir rutas de usuario, y servir el frontend estático. Se añadió una ruta de prueba y se configuró el manejo de rutas para servir el archivo index.html del frontend en todas las rutas no especificadas. Estos cambios aseguran una mejor integración entre el frontend y el backend, y permiten que el servidor maneje tanto las API como las solicitudes del frontend.
    
    - Middleware de Análisis del Cuerpo: Permite al servidor analizar JSON en las solicitudes entrantes.
    - Conexión a MongoDB: Asegura que el servidor se conecte a la base de datos MongoDB al iniciar.
    - Definición de Rutas de Usuario: Incluye las rutas necesarias para manejar las operaciones relacionadas con usuarios.
    - Ruta de Prueba: Añade una ruta de prueba para verificar la funcionalidad básica del servidor.
    - Servir Frontend Estático: Configura el servidor para servir archivos estáticos del frontend y manejar todas las demás rutas sirviendo el archivo index.html

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDB from './db';
 import dotenv from 'dotenv';
+import path from 'path';
 import userRoutes from './api/routes/user.routes';
 
 // Cargar las variables de entorno del archivo .env.development
@@ -9,16 +10,31 @@ dotenv.config({ path: '../client/.env.development' });
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware para parsear el cuerpo de las solicitudes
+app.use(express.json());
+
 // Conectar a MongoDB
 connectDB();
 
-// Definir 
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+// Definir una ruta de prueba
+app.get('/api/user/test', (req, res) => {
+  res.json({ message: 'Ruta de prueba funcionando' });
 });
 
-// Rutas
+// Montar las rutas de usuario
 app.use('/api/user', userRoutes);
 
+// Servir el frontend estático
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Manejar todas las demás rutas con el frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
 
 export default app;
