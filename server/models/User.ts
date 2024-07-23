9449aba6c145f5c1f7241c18cdc4ba92cfc9432e
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-07-23 10:41:59 +1000

    feat: actualizar esquema de usuario con middleware y métodos de contraseña
    Actualizado el esquema de usuario en mongoose para incluir middleware de encriptación de contraseñas antes de guardar y un método para comparar contraseñas. Se añadió soporte de timestamps para registrar las fechas de creación y actualización. Estos cambios mejoran la seguridad y funcionalidad del modelo de usuario.
    
    - Middleware de Encriptación de Contraseñas: Encripta la contraseña del usuario antes de guardar el documento en la base de datos, utilizando bcrypt.
    - Método para Comparar Contraseñas: Permite comparar contraseñas en el proceso de autenticación, utilizando bcrypt para verificar si la contraseña proporcionada coincide con la almacenada.
    - Soporte de Timestamps: Añade timestamps para registrar automáticamente las fechas de creación y actualización del documento.
    - Tipado Mejorado: Asegura que el esquema está correctamente tipado como Schema<IUser>.

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,20 +1,37 @@
-import mongoose, {Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { IUser } from '../common/Interfaces';
+import bcrypt from 'bcrypt';
 
-// Deficion del esquema para el usuario
-const userSchema: Schema = new Schema({
-  uid:{type:String,required:true, unique:true},
+// Definición del esquema para el usuario
+const userSchema: Schema<IUser> = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String },
-  age: { type: Number},
+  age: { type: Number },
   password: { type: String, required: true },
   resetPasswordToken: { type: String },
   resetPasswordExpires: { type: Number },
+}, { timestamps: true });
+
+// Middleware para encriptar la contraseña antes de guardar el usuario
+userSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
 });
 
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
 // Exporta el modelo de usuario
-const User = mongoose.model<IUser>('User', userSchema);
+const User = mongoose.model<IUser>('User', userSchema, 'usuarios');
 
 export default User;
